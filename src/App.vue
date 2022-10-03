4917b770a7753ce1033ede978ba6f1a772929712
L3XxXa <alexkkk@inbox.ru>
2022-10-03 14:21:32 +0700

    ultra base

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div>
+        Это дерьмо поднялось
+    </div>
+    <div>
+        <button @click="addLike">
+            <strong>LIKE!!1!!!11</strong>
+        </button>
+    </div>
+    <div><strong>Количество лайков {{ likes }}</strong></div>
 </template>
 
+<script>
+export default{
+    data(){
+        return{
+            likes: 0,
+        }
+    },
+    methods: {
+        addLike(){
+            this.likes += 1
+        }
+    },
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
