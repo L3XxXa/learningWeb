61a48b9ca438e56e911a1db37b3e1b45e165d381
L3XxXa <alexkkk@inbox.ru>
2022-10-05 11:14:33 +0700

    Some changes

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -4,17 +4,15 @@
             Make new post
         </h4>
         <form @submit.prevent>
-            <input
+            <my-input
                 v-model="post.title"
                 type="text"
-                class="input" 
-                placeholder="Post name">
+                placeholder="Post name"/>
 
-            <input 
+            <my-input 
                 v-model="post.body"
                 type = "text"
-                class="input"
-                placeholder="Body">
+                placeholder="Body"/>
         </form>
         <my-button @click="createPost">Post!!!</my-button>
     </div>
@@ -49,13 +47,7 @@ export default{
         display: flex;
         flex-direction: column;
     }
-    .input{
-        padding: 15px;
-        border: 3px solid blueviolet;
-        margin-top: 10px;
-        font-size: 20px;
-        width: 100%;
-    }
+
     h4{
         font-size: 40px;
     }
---
+++ src/components/UI/MyInput.vue
@@ -0,0 +1,27 @@
+<template>
+    <input type="text" class="input" :value = "modelValue" @input="updateInput">
+</template>
+
+<script>
+    export default {
+        name: "my-input",
+        props:{
+            modelValue: [String, Number]
+        },
+        methods: {
+            updateInput(event){
+                this.$emit
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .input{
+        padding: 15px;
+        border: 3px solid blueviolet;
+        margin-top: 10px;
+        font-size: 20px;
+        width: 100%;
+    }
+</style>
