6b28a9cc719f3c2193d1318f9b775df6a9f1386c
L3XxXa <alexkkk@inbox.ru>
2022-10-05 12:18:11 +0700

    List of posts is empty

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div v-if="posts.length>0">
         <h4>List of posts</h4>
         <post-item 
         v-for="post in posts" 
@@ -8,6 +8,7 @@
         @remove="$emit('remove', post)"
         />        
     </div>
+    <h2 v-else>No posts</h2>
 </template>
 
 <script>
